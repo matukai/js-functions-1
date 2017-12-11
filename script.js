534973a7d31f5737566ccdda78a930c89c5200a6
matukai <matt.tanji@gmail.com>
2017-12-11 11:50:20 -1000

    commit branch

--- script.js
+++ script.js
@@ -6,17 +6,21 @@ function runAfterTests() {
    */
 
   // Create a variable called `myNumber`, and assign it to any number
-
+var myNumber = 10000;
   /**
    * Create another variable called `stringValue`.
    * Assign it the value that's returned when you pass
    * `myNumber into the function `numberToString`.
    */
-
+var stringValue = numberToString(myNumber);
+console.log(stringValue);
   /**
    * If you pass `myNumber` and `stringValue` into `areEqual`,
    * what value will you get back? Why is that?
    */
+
+console.log(areEqual(myNumber,stringValue));
+//you get false because it is a different data type. string vs integer.
 
   /**
    * You may have noticed that the `calculate` function combined
@@ -24,6 +28,8 @@ function runAfterTests() {
    * Did you try to use those functions _inside_ of `calculate`?
    * If you didn't go back and try it, this is called "reusability"!
    */
+
+console.log(calculate("divide",1000,4));
 
   // Do you see any other functions where code could be reused?
 
@@ -34,6 +40,9 @@ function runAfterTests() {
    * Does it still work as expected? If it doesn't, how could you improve this?
    */
 
+//added another 'else if' statement to function if less than 0 grade = 'F'
+console.log(letterGrade(-100000,100));
+
   /**
    * Your `combine` function looks okay,
    * but what if you wanted to join it with ANY string instead of a single space?
@@ -42,8 +51,16 @@ function runAfterTests() {
    * third parameter called `glue` (a string value).
    */
 
+   function improvedCombine(word1,word2,glue){
+    return word1 + " " + glue + " " + word2;
+    }
+    console.log(improvedCombine("DROP","BASS","THE"));
   // What happens if you were to uncomment the following lines?
 
-  // combine = improvedCombine;
-  // console.log(combine('combine', 'improved!', ' is '));
+      combine = improvedCombine;
+      console.log(combine('combine', 'improved!', ' is '));
+ 
+
+
+
 }
